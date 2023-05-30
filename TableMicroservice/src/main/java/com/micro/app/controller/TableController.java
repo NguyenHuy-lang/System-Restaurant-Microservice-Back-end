7c48bbd3c8c6bbc37f65ddae473510eecde8c12e
ducanhptitb19cn028 <99241227+ducanhptitb19cn028@users.noreply.github.com>
2023-05-31 00:10:36 +0700

    done table

--- TableMicroservice/src/main/java/com/micro/app/controller/TableController.java
+++ TableMicroservice/src/main/java/com/micro/app/controller/TableController.java
@@ -4,19 +4,48 @@ import com.micro.app.model.Table;
 import com.micro.app.repository.TableRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tables")
 @RequiredArgsConstructor
+@CrossOrigin
 public class TableController {
     private final TableRepository tableRepository;
-    @GetMapping("/")
+    @GetMapping()
     ResponseEntity<List<Table>> getAllTable() {
         return ResponseEntity.ok().body(tableRepository.findAll());
+    }
+    @GetMapping("/{tableId}")
+    ResponseEntity<Table> getTableById(@PathVariable(value = "tableId") Integer tableId) {
+        Table table = tableRepository.findById(tableId).orElse(null);
+        if (table == null) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok().body(table);
+        }
+    }
+    @GetMapping("/status/{status-name}")
+    ResponseEntity<List<Table>> getTableByStatus(@PathVariable(value = "status-name") String status) {
+        List<Table> table = tableRepository.findByStatus(status);
+        if (table == null) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok().body(tableRepository.findByStatus(status));
+        }
+    }
+    @GetMapping("/capacity/{number}")
+    ResponseEntity<List<Table>> getTableByCapacity(@PathVariable Integer number) {
+        List<Table> table = tableRepository.findByCapacity(number);
+        if (table == null) {
+            return ResponseEntity.notFound().build();
+        }
+        else{
+            return ResponseEntity.ok().body(tableRepository.findByCapacity(number));
+        }
     }
 }
--- TableMicroservice/src/main/java/com/micro/app/repository/TableRepository.java
+++ TableMicroservice/src/main/java/com/micro/app/repository/TableRepository.java
@@ -3,7 +3,11 @@ package com.micro.app.repository;
 import com.micro.app.model.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface TableRepository extends JpaRepository<Table, Integer> {
-
+    List<Table> findByStatus(String status);
+    List<Table> findByCapacity(Integer capacity);
 }
